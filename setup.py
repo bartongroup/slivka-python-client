ec149b3581266360a07fc35d6c37e1ed0ebb2ab2
MMWarowny <mmzwarowny@dundee.ac.uk>
2020-03-10 15:20:43 +0000

    Fix setup configuration.

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@ from setuptools import setup
 
 setup(
     name='slivka-client',
-    version='1.0',
-    py_modules=['slivka_client'],
+    version='1.1',
+    packages=['slivka_client'],
     install_requires=[
-        'click>=7.0',
         'requests>=2.13.0',
-        'urllib3>=1.12'
-    ],
-    entry_points={
-        'console_scripts': [
-            'slivka=cli:main'
-        ]
-    }
+        'urllib3>=1.12',
+        'attrs>=19.0'
+    ]
 )
