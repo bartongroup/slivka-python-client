943b6f94a4fb875ce2f2280061512af41e4ebb31
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2021-06-14 16:46:38 +0200

    Reintroduced command line client.

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ setup(
     install_requires=[
         'requests>=2.13.0',
         'urllib3>=1.12',
-        'attrs>=19.0'
-    ]
+        'attrs>=19.0',
+        'click>=7.1.2'
+    ],
+    entry_points={
+        'console_scripts': [
+            "slivka-cli = slivka_client.__main__:main"
+        ]
+    }
 )
---
+++ slivka_client/__main__.py
@@ -0,0 +1,124 @@
+import collections
+import os
+import textwrap
+
+import attr
+import click
+
+from slivka_client import Service
+from .client import SlivkaClient
+
+
+@click.group()
+@click.version_option("1.1", prog_name='slivka-cli')
+@click.option("--host", metavar="URL", help="Slivka server url.",
+              default="https://www.compbio.dundee.ac.uk/slivka/")
+@click.pass_context
+def main(ctx: click.Context, host):
+    obj = ctx.ensure_object(dict)
+    obj['client'] = SlivkaClient(host)
+
+
+@main.command()
+@click.option("--name", help="Show one service by name.", metavar="NAME")
+@click.option("--terse", is_flag=True, help="Short output.")
+@click.pass_obj
+def services(obj, name, terse):
+    client: SlivkaClient = obj['client']
+    if name:
+        service = client.get_service(name)
+        _print_service(service, terse)
+    else:
+        for service in client.services:
+            _print_service(service, terse)
+
+
+def _print_service(service, terse=False):
+    if terse:
+        click.echo(service.name)
+    else:
+        click.echo(f"{service.name}: {service.label}")
+        click.echo("classifiers:")
+        text = textwrap.indent('\n'.join(service.classifiers), ' - ')
+        click.echo(text)
+        click.echo("fields:")
+        lines = []
+        for field in service.form:
+            attrs = attr.asdict(field, filter=lambda _, val: val is not None,
+                                dict_factory=collections.OrderedDict)
+            line = f"{attrs.pop('name')}: {attrs.pop('type').value}; "
+            line += ", ".join(
+                f"{key.replace('_', '-')}={val}"
+                for key, val in attrs.items()
+            )
+            lines.append(line)
+        text = textwrap.indent('\n'.join(lines), ' - ')
+        click.echo(text)
+
+
+@main.command()
+@click.option("--terse", is_flag=True, help="Short output.")
+@click.argument("service")
+@click.argument("values", nargs=-1, metavar="KEY=VALUE...")
+@click.pass_obj
+def submit(obj, service, values, terse):
+    client: SlivkaClient = obj['client']
+    service: Service = client.get_service(service)
+    form = service.new_form()
+    for arg in values:
+        k, v = arg.split('=', 1)
+        if v.startswith('@'):
+            form.set(k, open(v[1:], 'rb'))
+        else:
+            form.append(k, v)
+    jid = form.submit()
+    if terse:
+        click.echo(jid)
+    else:
+        click.echo(f"Job {jid} submitted successfully.")
+
+
+@main.command()
+@click.option("--terse", is_flag=True, help="Short output.")
+@click.argument("job-id")
+@click.pass_obj
+def status(obj, job_id, terse):
+    client: SlivkaClient = obj['client']
+    job_status = client.get_job_state(job_id)
+    if terse:
+        click.echo(job_status.name)
+    else:
+        click.echo(f"The job status is: {job_status.name}")
+
+
+@main.command()
+@click.option("--download/--no-download", is_flag=True, default=False,
+              help="Choose whether to download files.", show_default=True)
+@click.option("--directory", type=click.Path(file_okay=False, dir_okay=True),
+              default=os.curdir, help="Change download directory.")
+@click.option("--overwrite", default="prompt", show_default=True,
+              type=click.Choice(["yes", "no", "prompt"], case_sensitive=False),
+              help="Set overwriting behaviour for existing files.")
+@click.argument("job-id")
+@click.pass_obj
+def files(obj, job_id, download, directory, overwrite):
+    client: SlivkaClient = obj['client']
+    for file in client.get_job_results(job_id):
+        click.echo(f"{file.uuid}: {file.label}; "
+                   f"content-type={file.media_type}")
+        if download:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            fp = os.path.join(directory, file.title)
+            if os.path.exists(fp):
+                if overwrite == "prompt":
+                    if not click.confirm(f"File {fp} exists. Overwrite?"):
+                        click.echo("Skipping.")
+                        continue
+                elif overwrite == "no":
+                    click.echo(f"File {fp} exists. Skipping.")
+            file.dump(fp)
+
+
+if __name__ == '__main__':
+    main()
